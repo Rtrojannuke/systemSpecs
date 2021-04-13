68e032b5c8af426abc7cde7f092a699ece0f5814
Nuke <arimiyahmubarak@gmail.com>
2021-04-13 16:49:12 +0100

    added script to take in input

--- src/arr_taker/arr_taker.java
+++ src/arr_taker/arr_taker.java
@@ -5,7 +5,13 @@ import java.util.Scanner;
 public class arr_taker {
 	public static void main(String[] args) {
 		input_Taker mub = new input_Taker();
+		int size = mub.n;
+		String[] title = mub.titles_array();
+		String[] categories = mub.categories_array();
 		
+		for(int i=0; i<size; i++) {
+			System.out.print(title[i] + " ");
+		}
 	
 	}
 	
--- src/arr_taker/input_Taker.java
+++ src/arr_taker/input_Taker.java
@@ -3,5 +3,30 @@ package arr_taker;
 import java.util.Scanner;
 
 public class input_Taker {
+	//Variable to determine the size of the array to be taken in 
+	public int n;
+	Scanner pen = new Scanner(System.in);
 	
+	public String[] titles_array(){
+
+		System.out.print("Enter the size of array: ");
+		n = pen.nextInt();
+		System.out.println(n);
+		
+		String[] title_category = new String[n];
+		for(int i =0; i<n; i++) {
+			System.out.print("Enter first array value: ");
+			title_category[i] = pen.next();
+		}
+		return title_category;
+		
+	}
+	public String[] categories_array() {
+		String[] category_array = new String[n];
+		for(int j = 0; j<n; j++) {
+			System.out.print("Enter second array value: ");
+			category_array[j] = pen.next();
+		}
+		return category_array;
+	}
 }
